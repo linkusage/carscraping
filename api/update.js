0119d2a2163492c7a4b39e79be8671cb8d8ca269
linkusage <132535329+linkusage@users.noreply.github.com>
2025-11-09 23:19:37 -0500

    Update update.js

--- api/update.js
+++ api/update.js
@@ -4,12 +4,11 @@ const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method NotAllowed" });
+    return res.status(405).json({ error: "Method Not Allowed" });
   }
 
   const body = req.body;
 
-  // Save JSON in Upstash Redis
   await redis.set("precios", body);
 
   res.status(200).json({ ok: true });

a0683cbdd5186cc47e084211d80ba6c92c66dfcd
linkusage <132535329+linkusage@users.noreply.github.com>
2025-11-09 23:09:07 -0500

    Update update.js

--- api/update.js
+++ api/update.js
@@ -1,13 +1,16 @@
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
+
+const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method Not Allowed" });
+    return res.status(405).json({ error: "Method NotAllowed" });
   }
 
   const body = req.body;
 
-  await kv.set("precios", body);
+  // Save JSON in Upstash Redis
+  await redis.set("precios", body);
 
   res.status(200).json({ ok: true });
 }
